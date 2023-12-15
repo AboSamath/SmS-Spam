c9c51fee69219ad83119db9c3cc2ad4632f727eb
Abdou Samath YATTE <abdousamathyade@gmail.com>
2023-12-15 00:51:41 +0000

    add utils

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ st.markdown("")
 
 st.markdown("")
 
-st.markdown('<h5 style="background-color: white; color: #000000; border: 3px solid #990000; padding: 10px; text-align: center">This application is designed detects/classifies a SMS into SPAM or HAM (normal) based on the textual data using Natural Language Processing. </h5>', unsafe_allow_html=True)
+st.markdown('<h6 style="background-color: white; color: #000000; border: 3px solid #990000; padding: 10px; text-align: center">This application is designed detects/classifies a SMS into SPAM or HAM (normal) based on the textual data using Natural Language Processing. </h6>', unsafe_allow_html=True)
 
 
 st.markdown("")
 
-st.markdown('<h5 style="background-color: white; color: #000080; padding: 10px; text-align: center">Please Input an SMS example. </h5>', unsafe_allow_html=True)
+st.markdown('<h5 style="background-color: white; color: #000080; padding: 10px; text-align: center">Please Input a SMS example. </h5>', unsafe_allow_html=True)
 
 texte_utilisateur = st.text_area(" ")
 
